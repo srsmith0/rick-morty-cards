2c4f64ff905bc3634ed81b633d0019c47debfc0b
Shawn Smith <srsmith11@live.com>
2022-04-11 17:37:54 -0600

    added keys

--- src/components/Filter/category/InputGroup.js
+++ src/components/Filter/category/InputGroup.js
@@ -1,21 +1,23 @@
 
 const InputGroup = ({ name, changeID, total }) => {
-return <div className="input-group mb-3">
-  <select
-  onChange={(e) => changeID(e.target.value)}
-  className="form-select"
-  id={name}
-  >
-    <option value="1">Choose...</option>
-      {[...Array(total).keys()].map((epiNum, index) => {
-        return (
-          <option value={epiNum + 1}>
-            {name} - {epiNum + 1}
-          </option>
-        );
-      })}
-  </select>
-</div>
+  return (
+    <div className="input-group mb-3">
+      <select
+        onChange={(e) => changeID(e.target.value)}
+        className="form-select"
+        id={name}
+      >
+        <option value="1">Choose...</option>
+        {[...Array(total).keys()].map((epiNum, index) => {
+          return (
+            <option key={epiNum} value={epiNum + 1}>
+              {name} - {epiNum + 1}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 };
 
 export default InputGroup;
